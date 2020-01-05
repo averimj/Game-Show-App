6316ab6c0bae25ed5741d71c6120df1166ea1484
Averi Johnson <averimj@live.com>
2020-01-04 18:40:20 -0700

    created constructor methods for Game/Phrase classes"

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+ class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      new Phrase('Be courageous'),
+      new Phrase('No rain no flowers'),
+      new Phrase('What we think we become'),
+      new Phrase('Conquer from within'),
+      new Phrase('Im possible')
+   ];
+    this.activePhrase = null;
+  }
+}
