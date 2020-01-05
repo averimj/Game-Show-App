782de61ffa1fb4b2139c4ae37f14fe17a05b5069
Averi Johnson <averimj@live.com>
2020-01-04 22:31:34 -0700

    adds comments for each classes

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,13 +7,15 @@ class Phrase {
     const ul = document.querySelector('ul');
     const div = document.getElementById('banner');
 
+    // takes 'this.phrase' and turns it into all lower case form
+    const phraseLowers = this.phrase.toLowerCase();
 
-    const phraseStrings = this.phrase.toString();
-    const phraseLowers = phraseStrings.toLowerCase();
+    // splits each word into a single letter
     const phraseSplits = phraseLowers.split('');
 
     console.log(phraseSplits);
 
+    // iterates thru each letter and creates an <li> for that letter, then adds the appropiate className for that letter. Appends the <li> to the <ul> which is then appended to the <div>
     for(let i = 0; i < phraseSplits.length; i++){
       if(phraseSplits[i] === ' '){
         const li = document.createElement('li');
@@ -30,5 +32,6 @@ class Phrase {
     }
     console.log(div);
   }
-  
+
+
 }
