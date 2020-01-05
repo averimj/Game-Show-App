beb2853b87cb6e159b78e9f1773f44018b6da7cf
Averi Johnson <averimj@live.com>
2020-01-04 22:34:27 -0700

    adds 'click' eventListener to 'start button'

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 const phrase = new Phrase();
-const game = new Game();
+// const game = new Game();
+
+const game;
+
+const button = document.getElementById('btn__reset');
+
+button.addEventListener( 'click', game = new Game.startGame() )
